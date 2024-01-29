b3ea3e6d788fc655d8b177a6e0d170ad254ea1a9
UffeBC <113894934+UffeBC@users.noreply.github.com>
2024-01-29 14:20:10 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { FormEvent } from 'react'
+
+function AlbumPicker() {
+  console.log('AlbumPicker component rendered');
+  const [albums, setAlbums] = useState<string[]>([]);
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      artist: { value: string };
+    };
+    const artist = encodeURIComponent(target.artist.value);
+    const url = `https://musicbrainz.org/ws/2/release?fmt=json&query=artist:${artist}`;
+    const response = await fetch(url);
+    const mbResult = (await response.json()) as {
+      releases: { title: string }[];
+    };
+    const { releases } = mbResult;
+    setAlbums(releases.map(({ title }) => title));
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Artist name:
+        <input name="artist" />
+      </label>
+      <button type="submit">Search</button>
+      <p>Albums:</p>
+      <ol>
+        {albums.map((album) => (
+          <li>{album}</li>
+        ))}
+      </ol>
+    </form>
+  );
+}
+
+function App() {
+  console.log('App rendered ;)');
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+
+      <AlbumPicker />
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
